5a26f806f580d3f9603c5abf7947df5bdc35491e
LADJOUZI rachid <59792800+RashidLadj@users.noreply.github.com>
2020-05-04 12:47:30 +0200

    Implementing Point Light

---
+++ Scripts/11-pointLight&GUI.js
@@ -0,0 +1,110 @@
+function init() {
+	var scene = new THREE.Scene();
+	var gui = new dat.GUI();
+
+	var enableFog = false;
+	if (enableFog) {
+		scene.fog = new THREE.FogExp2(0xffffff, 0.2);
+	}
+	
+	var box = getBox(1, 1, 1);
+	var plane = getPlane(20);
+	var pointLight = getPointLight(1,'#ffffff');
+	var sphere = getSphere(0.05);
+
+	plane.name = 'plane-1';
+
+	box.position.y = box.geometry.parameters.height/2;
+	plane.rotation.x = Math.PI/2;
+	pointLight.position.set( 0, 2, 0 );
+	pointLight.intensity = 0.05;
+
+	scene.add(box);
+	scene.add(plane);
+	pointLight.add(sphere);
+	scene.add(pointLight);
+
+	gui.add(pointLight, 'intensity', 0, 1);
+	gui.addColor(pointLight, 'color');
+	gui.add(pointLight.position, 'x', -5, 5);
+	gui.add(pointLight.position, 'y', -5, 5);
+	gui.add(pointLight.position, 'z', -5, 5);
+
+	var camera = new THREE.PerspectiveCamera(
+		45,
+		window.innerWidth/window.innerHeight,
+		1,
+		1000
+	);
+
+	camera.position.x = 1;
+	camera.position.y = 2;
+	camera.position.z = 5;
+
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor('rgb(120, 120, 120)');
+	update(renderer, scene, camera);
+
+	document.getElementById('webgl').appendChild(renderer.domElement);
+}
+
+function getBox(w, h, d) {
+	var geometry = new THREE.BoxGeometry(w, h, d);
+	var material = new THREE.MeshPhongMaterial({
+		color: 'rgb(120, 120, 120)'
+	});
+	var mesh = new THREE.Mesh(
+		geometry,
+		material 
+	);
+
+	return mesh;
+}
+
+function getPlane(size) {
+	var geometry = new THREE.PlaneGeometry(size, size);
+	var material = new THREE.MeshPhongMaterial({
+		color: 'rgb(120, 120, 120)',
+		side: THREE.DoubleSide
+	});
+	var mesh = new THREE.Mesh(
+		geometry,
+		material 
+	);
+
+	return mesh;
+}
+
+function getSphere(size) {
+	var geometry = new THREE.SphereGeometry(size, 24, 24);
+	var material = new THREE.MeshBasicMaterial({
+		color: 'rgb(255, 255, 255)'
+	});
+	var mesh = new THREE.Mesh(
+		geometry,
+		material 
+	);
+
+	return mesh;
+}
+
+function getPointLight(intensity, color) {
+	var light = new THREE.PointLight(color, intensity);
+	return light;
+}
+
+function update(renderer, scene, camera) {
+	renderer.render(
+		scene,
+		camera
+	);
+
+	requestAnimationFrame(function() {
+		update(renderer, scene, camera);
+	})
+}
+
+init();
